e9266817847349d07adc793ab6978a9dfc57ae3c
hiltas73 <h_iltas@yahoo.com>
2024-12-05 19:54:16 +0300

    BasePage.java DONE

---
+++ src/test/java/com/demo/pages/BasePage.java
@@ -0,0 +1,19 @@
+package com.demo.pages;
+
+import com.demo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//a[text()=' Signup / Login']")
+    public WebElement loginBtn;
+
+
+
+}

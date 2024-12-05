77911be204afc130e5c558a4ebd2ee7bd75a17a9
hiltas73 <h_iltas@yahoo.com>
2024-12-05 19:53:14 +0300

    Hooks.java DONE

---
+++ src/test/java/com/demo/steps/Hooks.java
@@ -0,0 +1,26 @@
+package com.demo.steps;
+
+import com.demo.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        // Set up any necessary setup for each scenario
+        // maximize page and implicitly wait is setup in Driver class
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+    }
+
+}

786fc6fd1633b589e420c5f7697d160d3dd28c38
hiltas73 <h_iltas@yahoo.com>
2024-12-05 15:08:30 +0300

    Runner.java DONE

---
+++ src/test/java/com/demo/runners/Runner.java
@@ -0,0 +1,21 @@
+package com.demo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features" ,
+        glue = "com/example/steps",
+        dryRun = false,
+        tags = "@smoke"
+)
+public class Runner {
+}
